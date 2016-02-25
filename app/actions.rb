8ea8452f550fcbc2700b3e769f4dec49eb11b9e0
Reinhard Simeon <reinhardsimeon@gmail.com>
2016-02-25 18:34:41 +0000

    remove comments

--- app/actions.rb
+++ app/actions.rb
@@ -3,13 +3,6 @@ get '/' do
   erb :index
 end
 
-# maybe put it under matches folder, and call 
-# this erb file: show.erb?
-
-# because naming a 'matches' is 
-# overlapping with matches folder
-
-
 get '/users/:id' do
   erb :'/users/matches'
 end
