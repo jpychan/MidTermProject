b96cf9a7d1644b5c5dafbdfcdb7f3b4d0430f2ce
jpychan <jennychan234@gmail.com>
2016-02-25 13:09:34 -0800

    added routes, sign up form, log in form

--- app/actions.rb
+++ app/actions.rb
@@ -1,16 +1,102 @@
+helpers do
+  def current_user
+    @current_user = User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def check_flash
+    @flash = session[:flash] if session[:flash]
+    session[:flash] = nil
+  end
+
+end
+
+before do
+  current_user
+  check_flash
+end
+
 # Homepage (Root path)
 get '/' do
   erb :index
 end
 
+#User Login / Logout
+get '/users/login' do
+  erb :'users/login'
+end
+
+post '/users/login' do
+  @user = User.find_by(:username => params[:username])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    session[:flash] = "Invalid login."
+    redirect '/users/login'  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
+
+#User Sign Up
+
+get '/users/signup' do
+  @user = User.new
+  erb :'users/signup_form'
+end
+
+post '/users/signup' do
+  username = params[:username]
+  name = params[:name]
+  password = params[:password]
+
+  @user = User.new username: username, name: name, password: password
+
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    session[:flash] = "We could not create your user. You suck."
+    redirect '/users/signup'
+  end
+end
+
+
+#Matches Views
 get '/matches' do
   erb :'matches/index'
 end
 
 get '/matches/new' do
+  @match = Match.new
   erb :'matches/new'
 end
 
-get '/users/:id' do
+# post '/matches/record' do
+#   @match = Match.new
+#   if @match.save
+#     redirect '/matches'
+#   else
+#     session[:flash] = "There was an error."
+#     redirect 'matches/new'
+#   end
+# end
+
+get '/matches/edit' do
+  @match = Match.find(:id)
+  erb :'matches/edit'
+end
+
+post '/matches/edit' do
+  #insert code from new match form when I have it
+end
+
+
+get '/users/' do
   erb :'/users/matches'
+end
+
+post '/matches/user/reset' do
 end
--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ require 'active_support/all'
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/contrib/all' # Requires cookies, among other things
-
+require 'bcrypt'
 require 'pry'
 
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
