b5bce4d622bb5527bbdfd95125f698e0563281a0
Reinhard Simeon <reinhardsimeon@gmail.com>
2016-02-25 05:25:30 +0000

    static page friend-matches

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,14 @@
 get '/' do
   erb :index
 end
+
+# maybe put it under matches folder, and call 
+# this erb file: show.erb?
+
+# because naming a 'matches' is 
+# overlapping with matches folder
+
+
+get '/users/:id' do
+  erb :'/users/matches'
+end
