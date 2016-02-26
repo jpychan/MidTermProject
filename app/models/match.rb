b2ef1253fae25c970c83615b120711ef8dc85716
quicklikerabbit <steven.d.richards@gmail.com>
2016-02-26 00:26:46 +0000

    Updated user and match models

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,8 @@
 class Match < ActiveRecord::Base
 
+# validate that player 1 and player 2 are not the same
+# validate that winner and loser aren't the same
+
 belongs_to :game
 
 belongs_to :player1,  class_name: "User",
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
-  
-  has_many :matches
+
   has_many :reset_requests
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
-  
+
+  def matches
+    Match.where("player1_id = ? OR player2_id = ?", self.id, self.id)
+  end
+
 end
