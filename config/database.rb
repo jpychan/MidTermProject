651c3e413de06ad1328e13eac2631f47b773eac0
quicklikerabbit <steven.d.richards@gmail.com>
2016-03-01 01:00:13 +0000

    modified files to allow for production database

--- config/database.rb
+++ config/database.rb
@@ -16,4 +16,13 @@ configure do
     autoload ActiveSupport::Inflector.camelize(filename), model_file
   end
 
+  if development?
+    set :database, {
+      adapter: "sqlite3",
+      database: "db/db.sqlite3"
+    }
+  else
+    set :database, ENV['DATABASE_URL']
+  end
+
 end
